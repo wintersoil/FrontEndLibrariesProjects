83abbe0a6b406862944a45da72ae1d4660e1685b
Ali Usmani <37000081+wintersoil@users.noreply.github.com>
2018-09-25 22:34:56 -0500

    Update script.js

--- RandomQuoteMachine/script.js
+++ RandomQuoteMachine/script.js
@@ -2,68 +2,6 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src','https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js');
 document.head.appendChild(jQueryScript);
 
-let lib = [
-  {
-    quote: "Everything has beauty, but not everyone can see.",
-    author: "Confucius"
-  },
-  {
-    quote: "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
-    author: "Dalai Lama"
-  },
-  {
-    quote: "You can't fall if you dont climb. But there's no joy in living your whole life on the ground.",
-    author: "Unknown"
-  },
-  {
-    quote: "The best revenge is massive success.",
-    author: "Frank Sinatra"
-  },
-  {
-    quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
-    author: "Chinese Proverb"
-  },
-  {
-    quote: "It does not matter how slowly you go as long as you do not stop.",
-    author: "Confucius"
-  },
-  {
-    quote: "Nothing is impossible, the word itself says, \"I'm possible!\"",
-    author: "Audrey Hepburn"
-  },
-  {
-    quote: "First, have a definite, clear practical ideal; a goal, an objective. Second, have the necessary means to achieve your ends; wisdom, money, materials, and methods. Third, adjust all your means to that end.",
-    author: "Aristotle"
-  },
-  {
-    quote: "Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.",
-    author: "Jamie Paolinetti"
-  },
-  {
-    quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
-    author: "Amelia Earhart"
-  },
-  {
-    quote: "The question isn't who is going to let me; it's who is going to stop me.",
-    author: "Ayn Rand"
-  },
-  {
-    quote: "Every strike brings me closer to the next home run.",
-    author: "Babe Ruth"
-  },
-  {
-    quote: "There are no traffic jams along the extra mile.",
-    author: "Roger Staubach"
-  },
-  {
-    quote: "Certain things catch your eye, but pursue only those that capture the heart.",
-    author: "Ancient Indian Proverb"
-  },
-  {
-    quote: "A truly rich man is one whose children run into his arms when his hands are empty.",
-    author: "Unknown"
-  }
-];
 $(document).ready(function(){
   let index = Math.floor(Math.random() * lib.length);
   let quote = lib[index].quote;
